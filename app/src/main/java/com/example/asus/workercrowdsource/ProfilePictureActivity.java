81181495bbd95655132d36fae738b0e957a32e91
Ayush <Ayush456@gmail.com>
2019-03-18 02:34:02 +0530

    Taking front picture in ProfilePictureAct is working but storing in database is still not implemented

--- app/src/main/java/com/example/asus/workercrowdsource/ProfilePictureActivity.java
+++ app/src/main/java/com/example/asus/workercrowdsource/ProfilePictureActivity.java
@@ -1,7 +1,11 @@
 package com.example.asus.workercrowdsource;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,37 +14,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.ByteArrayOutputStream;
 
 public class ProfilePictureActivity extends AppCompatActivity {
     private ImageButton mProfilePicture;
-
     private String username, password;
+    private byte[] data2;
     private Button mSave;
     private Uri mImageUri;
     private StorageReference mStorage;
+    UploadTask uploadTask;
+    Bitmap imageBitmap;
     private static final int GALLERY_INTENT =2;
+    private ProgressBar mProgressBar;
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int REQUEST_CAPTURE_IMAGE = 1888;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_picture);
 
-        mStorage = FirebaseStorage.getInstance().getReference(); // storage ref declared
 
-
-        mSave = (Button) findViewById(R.id.button6);
-        mProfilePicture = (ImageButton)findViewById(R.id.imageButton);
+        mProfilePicture = findViewById(R.id.imageButton);
+        mProgressBar = findViewById(R.id.progressBar3);
+        mProgressBar.setVisibility(View.INVISIBLE);  //making progressBar invisible
+        mSave = findViewById(R.id.button6);
 
         mProfilePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent,GALLERY_INTENT);
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
             }
         });
@@ -48,37 +68,27 @@ public class ProfilePictureActivity extends AppCompatActivity {
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                    startPosting();
-
-
+                mProgressBar.setVisibility(View.VISIBLE);
+                startPostingImage(imageBitmap);
+                mProgressBar.setVisibility(View.INVISIBLE);
             }
         });
 
     }
 
-    private void startPosting() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            imageBitmap = (Bitmap) data.getExtras().get("data");
+            mProfilePicture.setImageBitmap(imageBitmap);
 
 
-        StorageReference filepath = mStorage.child("profile_pic").child(mImageUri.getLastPathSegment());
-//        filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//            @Override
-//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//
-//            }
-//        });
-        Bundle bundle = getIntent().getExtras();
-        startActivity(new Intent(ProfilePictureActivity.this,EmailAndPassActivity.class),bundle);
+        }
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void startPostingImage(Bitmap photo){
 
-        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
-            mImageUri = data.getData();
-            mProfilePicture.setImageURI(mImageUri);
-        }
     }
 }
