b5320df2ea4a28420ee1fe9ff380e18bcff27144
Ayush <Ayush456@gmail.com>
2019-03-20 04:03:36 +0530

    Created 3 bottom navigation bar but not linked it

--- app/src/main/java/com/example/asus/workercrowdsource/UserHomeActivity.java
+++ app/src/main/java/com/example/asus/workercrowdsource/UserHomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.asus.workercrowdsource;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class UserHomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class UserHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        BottomNavigationView mBottomNav = findViewById(R.id.user_bottom_nav_id);
+
+        mBottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.view_recommended_job:
+
+                        break;
+
+                    case R.id.view_contractors:
+
+                        break;
+
+                    case R.id.view_profile:
+                        startActivity(new Intent(UserHomeActivity.this,MyProfileActivity.class));
+                        break;
+
+                }
+            }
+        });
+
     }
 }
