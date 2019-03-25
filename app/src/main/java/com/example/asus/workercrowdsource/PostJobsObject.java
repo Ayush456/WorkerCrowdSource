8e8828982ffc9b6e1d644fe503bba5ffe91e5151
Ayush <Ayush456@gmail.com>
2019-03-25 05:55:00 +0530

    Added RecyclerView on JobsPost for user but some elements don't show the data

---
+++ app/src/main/java/com/example/asus/workercrowdsource/PostJobsObject.java
@@ -0,0 +1,62 @@
+package com.example.asus.workercrowdsource;
+
+public class PostJobsObject {
+
+    private String Address;
+    private String EndDate;
+    private String StartDate;
+    private String Salary;
+    private String Job;
+    private String EstNoOfWorker;
+    public PostJobsObject() {
+    }
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(String endDate) {
+        EndDate = endDate;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String startDate) {
+        StartDate = startDate;
+    }
+
+    public String getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(String salary) {
+        Salary = salary;
+    }
+
+    public String getJob() {
+        return Job;
+    }
+
+    public void setJob(String job) {
+        Job = job;
+    }
+
+    public String getEstNoOfWorker() {
+        return EstNoOfWorker;
+    }
+
+    public void setEstNoOfWorker(String estNoOfWorker) {
+        EstNoOfWorker = estNoOfWorker;
+    }
+
+
+}
--- app/src/main/java/com/example/asus/workercrowdsource/UserHomeActivity.java
+++ app/src/main/java/com/example/asus/workercrowdsource/UserHomeActivity.java
@@ -1,20 +1,52 @@
 package com.example.asus.workercrowdsource;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserHomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDB;
+    DatabaseReference mUserJobs;
+    private FirebaseUser mUser;
+    private String myUiD="",myJobSal="",myAddr="",myJobStart="",myJobEnd="",myJobCount="";
+    private RecyclerView MyJobsList;
+
+    FirebaseRecyclerOptions<PostJobsObject> options;
+    FirebaseRecyclerAdapter firebaseRecyclerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        mDB = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        myUiD = mUser.getUid();
+        mUserJobs = mDB.getReference().child("USER_POST_JOBS").child(myUiD);
+
+        MyJobsList = (RecyclerView)findViewById(R.id.rv_show_jobs);
+        MyJobsList.setHasFixedSize(true);
+        MyJobsList.setLayoutManager(new LinearLayoutManager(this));
 
         BottomNavigationView mBottomNav = findViewById(R.id.user_bottom_nav_id);
 
@@ -25,6 +57,8 @@ public class UserHomeActivity extends AppCompatActivity {
                     startActivity(new Intent(UserHomeActivity.this,UserProvideJobsActivity.class));
                }else if(menuItem.getItemId() == R.id.view_userjob){
 
+
+
                }else {
                    startActivity(new Intent(UserHomeActivity.this,ProfileDetailsActivity.class));
                }
@@ -33,4 +67,82 @@ public class UserHomeActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        options = new FirebaseRecyclerOptions.Builder<PostJobsObject>()
+                        .setQuery(mUserJobs, PostJobsObject.class)
+                        .build();
+
+       firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostJobsObject, PostViewHolder>(options) {
+          @Override
+          protected void onBindViewHolder(@NonNull PostViewHolder holder, int position, @NonNull PostJobsObject model) {
+              holder.setAddress(model.getAddress());
+              holder.setEndDate(model.getEndDate());
+              holder.setStartDate(model.getStartDate());
+              holder.setJobTitle(model.getJob());
+              holder.setWorkerCount(model.getEstNoOfWorker());
+              holder.setSalary(model.getSalary());
+          }
+
+
+
+          @NonNull
+          @Override
+          public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+             View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_user_posted_job,viewGroup,false);
+
+              return new PostViewHolder(view);
+          }
+      };
+
+        firebaseRecyclerAdapter.startListening();
+        MyJobsList.setAdapter(firebaseRecyclerAdapter);
+
+
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder{
+       View mView;
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+       public void setJobTitle(String Title){
+           TextView jobTitle = mView.findViewById(R.id.post_job_title);
+           jobTitle.setText(Title);
+       }
+
+       public void setSalary(String Sal){
+            TextView post_sal = mView.findViewById(R.id.post_salary);
+            post_sal.setText(Sal);
+       }
+
+       public void setAddress(String Addr){
+            TextView post_addr = mView.findViewById(R.id.post_address);
+            post_addr.setText(Addr);
+       }
+
+       public void setWorkerCount(String count){
+            TextView post_worker_count = mView.findViewById(R.id.post_worker_count);
+       }
+
+       public void setContactNo(String ContactNo){
+            TextView post_contactNo = mView.findViewById(R.id.post_contact_no);
+       }
+       public void setEmailId(String Email){
+            TextView post_email_id = mView.findViewById(R.id.post_email);
+       }
+       public void setStartDate(String StartDate){
+            TextView post_start_date = mView.findViewById(R.id.post_start_date);
+       }
+       public void setEndDate(String EndDate){
+            TextView post_end_date = mView.findViewById(R.id.post_end_date);
+       }
+
+    }
 }
